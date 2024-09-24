b0cefbdf9b1cfa5e5aaabe3c318b805fe0ef40d2
Alexander <avboyko87@mail.ru>
2024-09-24 12:47:35 +0400

    fix модалка

--- public/admin/assets/scripts/index.js
+++ public/admin/assets/scripts/index.js
@@ -20,9 +20,33 @@ const overlay = document.getElementById("overlay");
 
 if (modal) {
   openModalBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
+    btn.addEventListener("click", (event) => {
       event.preventDefault();
-      modal.showPopover(); // Используем showPopover для открытия модального окна
+
+        if (btn.dataset.remove) {
+            // Находим блок modal-buttons в модалке
+            const modalButtons = modal.querySelector(".modal-buttons");
+
+            // Удаляем предыдущую радио-кнопку, если она уже есть
+            const existingRadio = modalButtons.querySelector(
+                'input[name="remove"]'
+            );
+            if (existingRadio) {
+                existingRadio.remove();
+            }
+
+            // Создаем новую радио-кнопку
+            const radioBtn = document.createElement("input");
+            radioBtn.type = "radio";
+            radioBtn.name = "remove";
+            radioBtn.value = btn.dataset.remove;
+            radioBtn.checked = true;
+
+            // Вставляем радио-кнопку в блок modal-buttons
+            modalButtons.appendChild(radioBtn);
+        }
+
+        modal.showPopover(); // Используем showPopover для открытия модального окна
       overlay.style.display = "block"; // Показываем затемнение фона
     });
   });
