72579c14751e670c5a0ddf6ce0600878828184e5
Alexander <avboyko87@mail.ru>
2024-12-09 12:46:36 +0400

    Refactoring team

---
+++ resources/views/team-element.blade.php
@@ -0,0 +1,9 @@
+<div class="team-people-element">
+    <img
+        class="team-people-element__image"
+        src="./assets/images/team/{{$filename}}"
+        alt="team"
+    />
+    <span class="team-people-element__name">{{$name}}</span>
+    <span class="team-people-element__who">{{$job}}</span>
+</div>
